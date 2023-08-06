425d4b4a7083f4c774bd4234a643fabd3530cf06
Erika Miguel <erika@erikamiguel.com>
2023-08-05 23:00:21 -0400

    Frontend improvements (#7)
    * enter key event
    
    * emotion styling
    
    * Small frontend improvements

--- app/index.js
+++ app/index.js
@@ -1,5 +1,25 @@
 import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
+import styled from '@emotion/styled'
+
+const Button = styled.button`
+  background-color: #008CBA; 
+  color: white; 
+  border: 2px solid #008CBA;
+  border-radius: 3px;
+  padding: 10px;
+`;
+
+const Input = styled.input`
+  padding: 10px;
+  width: 50%;
+  margin-right: 5px;
+`
+
+const Heading = styled.h2`
+  font-family: Arial;
+  color: gray;
+`;
 
 const App = () => {
   const [tag, setTag] = useState('');
@@ -9,25 +29,32 @@ const App = () => {
     setTag(event.target.value);
   };
 
+  const handleKeyPress = async (event) => {
+    if (event.key === 'Enter') {
+      await getCreationDate(tag);
+    }      
+  }
+
   const getCreationDate = async (tag) => {
     const response = await fetch(`/tag/${tag}`);
     if (response.status >= 400) {
       setCreationDate('Tag does not exist');
     } else {
       const data = await response.json();
-      setCreationDate(data.created_at);
+      setCreationDate(new Date(data.created_at).toLocaleDateString());
     }
   }
 
   return (
     <div>
-      <input type='text' 
-        placeholder='Search for a tag in the blockapps/strato-getting-started repo' 
+      <Input type='text' 
+        placeholder='Search for a tag in the blockapps/strato-getting-started repository' 
         value={tag}
         onChange={handleInputChange}
+        onKeyDown={handleKeyPress}
       />
-      <button onClick={() => getCreationDate(tag)}>Get Tag Creation Date</button>
-      <h2>Creation date: {creationDate}</h2>
+      <Button onClick={() => getCreationDate(tag)}>Get Tag Creation Date</Button>
+      <Heading>Creation date: {creationDate}</Heading>
     </div>
   );
 };
